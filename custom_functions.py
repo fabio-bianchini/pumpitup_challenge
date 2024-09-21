888fbf0c198e1f3d78a0b9a4bb32915bced0e1fb
fabio-bianchini <fabiobianchinicano@gmail.com>
2024-09-21 15:32:05 +0100

    first commit

---
+++ custom_functions.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import numpy as np
+
+def describe_numerical(df):
+  #select numerical columns
+  dates = df.select_dtypes(include = ['datetime64[ns]']).columns.tolist()
+  numerical = df.select_dtypes(include = ['int', 'Int64', 'float']).columns.tolist()
+  numerical += dates
+  
+  #data type
+  data_types = pd.DataFrame(
+    df[numerical].dtypes,
+    columns=['Data Type'])
+  
+  #missing data
+  missing_data = pd.DataFrame(
+    df[numerical].isnull().sum(),
+    columns=['Missing Values'])
+  
+  #unique values
+  unique_values = pd.DataFrame(
+    columns=['Unique Values'])
+  for row in list(df[numerical].columns.values):
+    unique_values.loc[row] = [df[numerical][row].nunique()]
+    
+    #number of records
+  count_values = pd.DataFrame(
+    columns=['Records'])
+  for row in list(df[numerical].columns.values):
+      count_values.loc[row] = [df[numerical][row].count()]
+    
+    #minimum value
+  maximum_values = pd.DataFrame(
+    columns=['Maximum Value'])
+  for row in list(df[numerical].columns.values):
+      maximum_values.loc[row] = [df[numerical][row].max()]
+    
+  #maximum value
+  minimum_values = pd.DataFrame(
+    columns=['Minimum Value'])
+  for row in list(df[numerical].columns.values):
+      minimum_values.loc[row] = [df[numerical][row].min()]
+    
+  #mean value
+  mean_values = pd.DataFrame(
+    columns=['Mean Value'])
+  for row in list(df[numerical].columns.values):
+      mean_values.loc[row] = [df[numerical][row].mean()]
+    
+  #first quartile
+  quartile_1 = pd.DataFrame(
+    columns=['1st Quartile'])
+  for row in list(df[numerical].columns.values):
+      quartile_1.loc[row] = [df[numerical][row].quantile(0.25)]
+    
+  #median
+  median = pd.DataFrame(
+  columns=['Median'])
+  for row in list(df[numerical].columns.values):
+    median.loc[row] = [df[numerical][row].quantile(0.5)]
+    
+  #third quartile
+  quartile_3 = pd.DataFrame(
+    columns=['3rd Quartile'])
+  for row in list(df[numerical].columns.values):
+    quartile_3.loc[row] = [df[numerical][row].quantile(0.75)]
+    
+  #join columns 
+  dq_report_num = data_types.join(count_values).join(missing_data).join(unique_values).join(minimum_values).join(maximum_values).join(mean_values).join(quartile_1).join(median).join(quartile_3)
+  
+  #percentage missing
+  dq_report_num['Missing %'] = (dq_report_num['Missing Values'] / len(df[numerical]) *100)
+  
+  # change order of columns
+  dq_report_num = dq_report_num[['Data Type', 'Records', 'Unique Values', 'Missing Values', 'Missing %', 'Mean Value', 'Minimum Value', '1st Quartile', 'Median', '3rd Quartile', 'Maximum Value', ]]
+
+  #round
+  dq_report_num[['Missing %', 'Mean Value', 'Minimum Value', '1st Quartile', 'Median', '3rd Quartile', 'Maximum Value']] =   dq_report_num[['Missing %', 'Mean Value', 'Minimum Value', '1st Quartile', 'Median', '3rd Quartile', 'Maximum Value']].round(1)
+
+  return(dq_report_num)
+
+
+def describe_categorical(df):
+  #select categorical columns
+  categorical = df.select_dtypes(include = 'object').columns.tolist()
+
+  #datatype
+  data_types = pd.DataFrame(
+    df[categorical].dtypes,
+    columns=['Data Type'])
+  
+  #count
+  count_values = pd.DataFrame(
+      columns=['Records'])
+  for row in list(df[categorical].columns.values):
+    count_values.loc[row] = [df[categorical][row].count()]
+  
+  #missing data
+  missing_data = pd.DataFrame(
+    df[categorical].isnull().sum(),
+    columns=['Missing Values'])
+  
+  #unique values
+  unique_values = pd.DataFrame(
+      columns=['Unique Values'])
+  for row in list(df[categorical].columns.values):
+    unique_values.loc[row] = [df[categorical][row].nunique()]
+
+  #mode
+  mode_values = pd.DataFrame(
+      columns=['Mode'])
+  for row in list(df[categorical].columns.values): 
+    mode_values.loc[row] = [df[categorical][row].mode()[0]]
+    mode = mode_values.loc[row]
+  
+  #mode frequency
+  listModeFreq = []
+  for row in categorical:
+    mode = df[row].mode().iat[0]
+    ModeFreq = df[row].value_counts()[mode]
+    #print(x, mode, df[x].value_counts()[mode])
+    listModeFreq .append(ModeFreq )
+  listModeFreq  = np.array(listModeFreq )
+    
+  #create data quality report
+  dq_report_cat = data_types.join(missing_data).join(count_values).join(unique_values).join(mode_values)
+  dq_report_cat['Mode freq.'] = listModeFreq 
+  dq_report_cat['Mode %'] = (dq_report_cat['Mode freq.'] / dq_report_cat['Records']*100) .astype('float')
+  dq_report_cat['Missing %'] = (dq_report_cat['Missing Values'] / len(df[categorical]) *100)
+
+  #change order of columns
+  dq_report_cat = dq_report_cat[['Data Type', 'Records', 'Unique Values', 'Missing Values', 'Missing %', 'Mode', 'Mode freq.', 'Mode %']]
+  dq_report_cat[['Missing %', 'Mode %']] =   dq_report_cat[['Missing %'	,  'Mode %']].round(1  )
+
+  return(dq_report_cat)
+
+
+def distribution_checker (data, xlabel):
+  grouped = data.groupby([xlabel, 'status_group'])['id'].count().reset_index()
+  pivot = grouped.pivot_table(index = xlabel, columns = 'status_group', fill_value = 0)
+  mi=pivot.columns
+  mi.tolist()
+  ind = pd.Index([ str(e[1])  for e in mi.tolist()])
+  pivot.columns = ind
+  pivot['Num_bombas'] = pivot['functional'] + pivot['functional needs repair'] + pivot['non functional']
+  pivot['Total_bombas'] = pivot['Num_bombas'].sum()
+  pivot['Porcentaje_total_bombas'] =((pivot['Num_bombas'] / pivot['Total_bombas']) * 100).round(1)
+  pivot['Porcentaje_funcional'] = ((pivot['functional'] / pivot['Num_bombas']) * 100).round(1)
+  pivot['Porcentaje_NO_funcional'] = ((pivot['non functional'] / pivot['Num_bombas']) * 100).round(1)
+  pivot['Porcentaje_reparacion'] = ((pivot['functional needs repair'] / pivot['Num_bombas']) * 100).round(1)
+  pivot = pivot.drop(['functional', 'functional needs repair', 'non functional', 'Total_bombas'], axis=1)
+  return(pivot)
+
+
+
+
+
+#replace string to integer
+data['public_meeting'] = data['public_meeting'].replace({True: 1, False: 0})
+data['imputed_permit'] = data['imputed_permit'].replace({True: 1, False: 0})
+
+
+#change to integer
+data[['imputed_gps_height', 'construction_year_imputed', 'imputed_population']] = data[['imputed_gps_height', 'construction_year_imputed', 'imputed_population']].astype('int')
+
+#change type to categorical
+data[['num_private', 'region_code', 'district_code', 'num_private']] = data[[ 'num_private', 'region_code', 'district_code', 'num_private']].astype('str')
+
+#remove decimal
+data['district_code'] = data['district_code'].str.split(".").str[0]
